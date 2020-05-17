3b083faafdd5aaec7b2839a31978849fe1a72045
Dominik Kaliszewski <kaliszewski.dominik@gmail.com>
2020-05-17 20:13:50 +0200

    Add global CORS.

--- src/main/java/org/fibi/usos/config/interceptor/InterceptorsConfiguration.java
+++ src/main/java/org/fibi/usos/config/interceptor/InterceptorsConfiguration.java
@@ -2,13 +2,20 @@ package org.fibi.usos.config.interceptor;
 
 import org.fibi.usos.interceptor.RoleAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class InterceptorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RoleAuthInterceptor());
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 }
