38509124c878db1e01de057792d5670a1e87b606
Dominik Kaliszewski <kaliszewski.dominik@gmail.com>
2020-05-17 21:26:28 +0200

    Add new endpoints test for GradeController.

--- src/test/java/org/fibi/usos/controller/course/grade/GradeControllerTests.java
+++ src/test/java/org/fibi/usos/controller/course/grade/GradeControllerTests.java
@@ -16,8 +16,9 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -62,6 +63,60 @@ public class GradeControllerTests {
         dto.setCreatedById(5);
         mockMvc.perform(post("/grade/add")
                 .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails("student")
+    public void getGradesForStudentWithValidRole() throws Exception{
+        mockMvc.perform(get("/grade/student/3"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+
+    @Test
+    @WithUserDetails("student")
+    public void getGradesForStudentWithValidRoleButNoGrades() throws Exception{
+        mockMvc.perform(get("/grade/student/666"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+
+    @Test
+    @WithUserDetails("lecturer")
+    public void getGradesForStudentWithNotValidRole() throws Exception{
+        mockMvc.perform(get("/grade/student/3"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails("lecturer")
+    public void getGradesForLecturerWithValidRole() throws Exception{
+        mockMvc.perform(get("/grade/lecturer/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+
+    @Test
+    @WithUserDetails("lecturer")
+    public void getGradesForLecturerWithValidRoleButNoGrades() throws Exception{
+        mockMvc.perform(get("/grade/lecturer/666"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+
+    @Test
+    @WithUserDetails("student")
+    public void getGradesForLecturerWithNotValidRole() throws Exception{
+        mockMvc.perform(get("/grade/lecturer/5"))
                 .andExpect(status().isUnauthorized());
     }
 }
