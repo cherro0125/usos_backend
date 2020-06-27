9350fa3b19e9cfeb7bd50009443672fdb1556b0d
Kasiaak <zuzanna.dawiec@gmail.com>
2020-06-27 15:31:55 +0100

    Fix DTO leak too much info about user

--- src/main/java/org/fibi/usos/dto/keys/KeyResponseDto.java
+++ src/main/java/org/fibi/usos/dto/keys/KeyResponseDto.java
@@ -3,12 +3,12 @@ package org.fibi.usos.dto.keys;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.fibi.usos.model.user.UserModel;
+import org.fibi.usos.dto.user.UserDto;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class KeyResponseDto {
     private String roomNumber;
-    private UserModel owner;
+    private UserDto owner;
 }
